cbe1adaa9a9c207e927ba96003d6edeaf8243be9
Leo <Leo@bouloc.eu>
2015-01-28 00:44:30 +0100

    new save

---
+++ meteo_scraper.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 12:28:18 2015
+
+@author: debian
+"""
+import requests
+from bs4 import BeautifulSoup
+from os.path import join
+import pandas as pd
+import time
+import datetime
+
+from multiprocessing import cpu_count
+from multiprocessing import Pool
+
+
+def get_page(dep, integer, date, return_list = True):
+    '''Renvoie une liste d infos si la page est trouvee, false sinon'''
+    integer = str(integer).zfill(3)
+    dep = str(dep).zfill(2) 
+    
+    page = requests.get('http://www.meteofrance.com/climat/meteo-date-passee?lieuId=' + str(dep) + str(integer) + '0&lieuType=VILLE_FRANCE&date=' + date)
+    data = page.text
+    soup = BeautifulSoup(data)     
+      
+    valid_code = 'style' in soup.find('div', {'id':'alertMessage'}).attrs
+    if valid_code:
+        zone_meteo = soup.find('div', {'class':'mod-article-wysiwyg wysiwyg-style-2 article-commentaire'})    
+        
+        info_mesure = zone_meteo.find('div', {'class':'mod-header'}).text
+        ville = info_mesure.split(u'à')[-1]
+        meteo = zone_meteo.find('ul')
+        
+        temp_min = zone_meteo.find('ul').find_all('li')[0].text.split(': ')[-1]
+        temp_min = temp_min.replace(u'°C', '')
+        
+        temp_max = zone_meteo.find('ul').find_all('li')[1].text.split(': ')[-1]
+        temp_max = temp_max.replace(u'°C', '')
+        
+        ensoleillement = zone_meteo.find('ul').find_all('li')[2].text.split(': ')[-1]
+        ensoleillement = ensoleillement.replace('h', '')    
+        
+        precipitations = zone_meteo.find('ul').find_all('li')[3].text.split(': ')[-1]
+        precipitations = precipitations.replace('mm', '')
+        
+        if return_list:
+            return [ville, dep, integer, date, temp_min, temp_max, ensoleillement, precipitations]
+        else:
+            table_index = ['ville', 'integer', 'date', 'temp_min', 'temp_max', 'ensoleillement', 'precipitations']
+            return pd.Series([ville, dep, integer, date, temp_min, temp_max, ensoleillement, precipitations], index = table_index)
+    else:
+        return valid_code
+        
+        
+
+# START : Script pour trouver 10 villes dans chaque déparement
+#table = pd.DataFrame()
+#departements = range(1,95)
+#for dep in departements:
+#    print '----->', dep
+#    villes_trouvees = 0
+#    integer = 0
+#    while villes_trouvees < 10:
+#        a = get_page(dep, integer, date)
+#        integer +=1
+#        if not isinstance(a, bool):
+#            print a['ville']
+#            table = table.append(a, ignore_index = True)
+#            villes_trouvees += 1
+#table = table[['ville', 'dep', 'integer', 'date', 'temp_min', 'temp_max', 'ensoleillement', 'precipitations']]
+# END : Script pour trouver 10 villes dans chaque déparement
+
+    
+
+
+def initiate(path, num_cores):
+    table_index = ['ville', 'dep', 'integer', 'date', 'temp_min', 'temp_max', 'ensoleillement', 'precipitations']
+    for i in range(num_cores):
+        f = open(join(path, 'table_meteo_' + str(i) + '.csv'), 'w')
+        for col in table_index[:-1]:
+            f.write(col + ';')
+        f.write(table_index[-1] + '\n')
+        f.close()
+
+#def get_all(path, liste_liste_des_dates, table_ref, indice):
+##    print 'Process', indice, 'has started'
+#    try:
+#        liste_des_dates = liste_liste_des_dates[indice]
+#        table_deja_faite = pd.read_csv(join(path, 'table_meteo_' + str(indice) + '.csv'), sep = ';')
+#        last_city = (table_deja_faite['integer'].iloc[-1], table_deja_faite['dep'].iloc[-1])
+#        f = open(join(path, 'table_meteo_' + str(indice) + '.csv'), 'a')
+#        all_int_dep = [x for x in zip(table_ref['integer'], table_ref['dep'])]
+#        for (integer, dep) in all_int_dep[all_int_dep.index(last_city):]:
+#            for date in liste_des_dates:
+#                for i in range(10):
+#                    try:
+#                        retour = get_page(dep, integer, date, return_list = True)
+#                        for col in retour[:-1]:
+#                            f.write(col + ';')
+#                        f.write(retour[-1] + '\n')
+#                        print retour
+#                        break
+#                    except Exception,e:
+#                        print dep, integer, date, 'was passed ...'
+#                        print '   >>>', e
+#                        time.sleep(30)
+#        'Process', indice, 'is finished'
+#    except:
+#        f.close()
+#        print 'Terminated process', indice,'before end. FILE CLOSED'
+        
+        
+def get_all_2(path, liste_a_faire, indice):
+#    print 'Process', indice, 'has started'
+    try:
+
+        f = open(join(path, 'table_meteo_' + str(indice) + '.csv'), 'a')
+        for (integer, dep, date) in liste_a_faire:
+            for i in range(10):
+                try:
+                    retour = get_page(dep, integer, date, return_list = True)     
+#                    print 'no error in retour', integer, dep, date
+                    if not isinstance(retour, bool):
+                        if len(retour) == 8:
+                            for col in retour[:-1]:
+                                f.write(col.encode('utf8') + ';')
+        #                        print 'no problem with col', col, integer, dep, date
+                            f.write(retour[-1] + '\n')
+                            print retour
+                        print dep, integer, date, 'had missing values'
+                    else:
+                        print dep, integer, date, 'had no values'
+                    break
+                except Exception, e:
+                    print dep, integer, date, 'was passed ...'
+                    print '   >>>', str(e)
+                    time.sleep(30)
+        'Process', indice, 'is finished'
+    except:
+        f.close()
+        print 'Terminated process', indice,'before end. FILE CLOSED'
+
+#def get_all_for_pool(indice):
+#    path = 'C:\\Users\\work\\Documents\\ETALAB_data\\meteo'
+#    csv_name = 'meteo.csv'
+#    num_cores = cpu_count()
+#    
+#    # Preparation des dates
+#    base = datetime.datetime(2009, 1, 1)
+#    liste_des_dates = [base + datetime.timedelta(days=x) for x in range(0, 2200)]
+#    liste_des_dates = [date.strftime('%d-%m-%Y') for date in liste_des_dates]
+##    liste_des_dates = [str(jour) + '-' + str(mois) + '-' + str(annee) for annee in range(2009,2015) for mois in range(1,13) for jour in range(1,32)]
+#    len_for_threading = len(liste_des_dates) // num_cores
+#    liste_liste_des_dates = [liste_des_dates[i*len_for_threading:(i+1)*len_for_threading] for i in range(num_cores)]
+#    
+#    # Preparations des variables integer
+#    table_ref = pd.read_csv(join(path, 'table_ref.csv'), sep = ';')
+#    table_ref = table_ref.groupby('dep').apply(lambda x: x.iloc[0])
+#    get_all(path, liste_liste_des_dates, table_ref, indice)
+    
+    
+def get_all_for_pool_2(indice):
+#    path = '/home/debian/Documents/data/meteo'
+    print "get_all started for process : ", indice
+    path = 'C:\\Users\\work\\Documents\\ETALAB_data\\meteo'
+    csv_name = 'meteo.csv'
+    num_cores = cpu_count()
+    table = load_global(path, 12)
+    
+    # Preparation des dates
+    base = datetime.datetime(2009, 1, 1)
+    liste_des_dates = [base + datetime.timedelta(days=x) for x in range(0, 2200)]
+    liste_des_dates = [date.strftime('%d-%m-%Y') for date in liste_des_dates]
+#    liste_des_dates = [str(jour) + '-' + str(mois) + '-' + str(annee) for annee in range(2009,2015) for mois in range(1,13) for jour in range(1,32)]
+    a_faire =  table.groupby(['dep', 'integer'])['date'].apply(lambda x: [date for date in liste_des_dates if not(date in list(x))]).reset_index()
+    a_faire_list = [(a_faire['integer'].iloc[i], a_faire['dep'].iloc[i], date) for i in range(len(a_faire)) for date in a_faire['date'].iloc[i]]    
+    len_for_threading = len(a_faire_list) // num_cores
+    liste_a_faire_list = [a_faire_list[i*len_for_threading:(i+1)*len_for_threading] for i in range(num_cores)]
+    get_all_2(path, liste_a_faire_list[indice], indice)
+        
+    
+    
+def load_partial_table(path, indice, to_correct = False):
+    table = pd.read_csv(join(path, 'table_meteo_' + str(indice) + '.csv'), sep = ';')
+    return table
+
+
+def load_global(path, num_cores):
+    table = pd.DataFrame()
+    for i in range(num_cores):
+        tab = load_partial_table(path, i)
+        table = table.append(tab)
+    table['date'] = table.date.apply(lambda date: '-'.join([date.split('-')[0].zfill(2), date.split('-')[1].zfill(2), date.split('-')[2]]))
+    return table
+    
+if __name__ == '__main__':
+#    path = '/home/debian/Documents/data/meteo'
+    path = 'C:\\Users\\work\\Documents\\ETALAB_data\\meteo'
+    csv_name = 'meteo.csv'
+    num_cores = cpu_count()
+    
+#    # Preparation des dates
+    base = datetime.datetime(2009, 1, 1)
+    liste_des_dates = [base + datetime.timedelta(days=x) for x in range(0, 2200)]
+    liste_des_dates = [date.strftime('%d-%m-%Y') for date in liste_des_dates]
+    len_for_threading = len(liste_des_dates) // num_cores
+    
+    table = load_global(path, 12)
+    grp = table.groupby(['dep', 'integer', 'ville', 'date'])
+    new_table = grp.apply(lambda x: x.iloc[0])
+    new_table = new_table.drop(['ville', 'dep', 'integer', 'date'], axis = 1).reset_index()        
+    
+    table_ref = pd.read_csv(join(path, 'table_ref.csv'), sep = ';')
+    table_ref = table_ref.groupby('dep').apply(lambda x: x.iloc[0])
+    table_ref['dep'] = range(1, 1+ len(table_ref))
+   
+    a_faire =  table.groupby(['dep', 'integer'])['date'].apply(lambda x: [date for date in liste_des_dates if not(date in list(x))]).reset_index()
+    a_faire_list = [(a_faire['integer'].iloc[i], a_faire['dep'].iloc[i], date) for i in range(len(a_faire)) for date in a_faire['date'].iloc[i]]
+    print 'Il reste :', len(a_faire_list), 'lignes à faire'
+#    len_for_threading = len(a_faire_list) // num_cores
+#    liste_a_faire_list = [a_faire_list[i*len_for_threading:(i+1)*len_for_threading] for i in range(num_cores)]
+#    liste_liste_des_dates = [liste_des_dates[i*len_for_threading:(i+1)*len_for_threading] for i in range(num_cores)]
+
+#    # Preparations des variables integer
+
+    
+###    initiate(path, num_cores)
+#    pool = Pool(processes = num_cores)
+#    pool.map(get_all_for_pool_2,range(num_cores))  
+
--- reperage_correlations.py
+++ reperage_correlations.py
@@ -9,78 +9,102 @@ Ce fichier contient :
     - Un script de réduction de table, en supprimant les colonnes pouvant être déduites d'autres colonnes,
         qui génère par ailleurs des prédicteurs capables de reconstituer la base initiale
     - Un script qui permet de sauvegarder les prédicteurs ainsi créés (TODO)
-    - Un script permettant de reconstruire la base initiale, ou seullement certaines variables (ou seulement... : TODO)
+    - Un script permettant de reconstruire la base initiale, ou seulement certaines variables (ou seulement... : TODO)
 #####################################################################################################
 """
 
-from DAMIR_CONFIG import nettoyage_fichier_R_1_path as path
+#from DAMIR_CONFIG import nettoyage_fichier_R_1_path as path
 from os.path import join
 import pandas as pd
 from itertools import combinations
-
-#table = pd.read_csv(join(path, 'reduced_R201411.CSV'), sep = ';')
-
-
-
-columns_to_analyse = [col for col in table.columns if not(col in ['Unnamed: 0', 'rem_mon', 'rec_mon', 'dep_mon', 'act_dnb', 'act_coe'])]
+from random import shuffle
 
 # TODO : Créer une nouvelle fonction pour les implications
 # TODO : enlever tous les apply : all(smaller_table.groupby(list_of_columns)[variable_to_predict].nunique() == 1)
+
 def check_all_implications(table, list_of_columns): 
     '''Checks if variable_to_predict is predicted by list_of_columns'''
-    implicates = [x for x in list(table.columns) if (not x in list_of_columns)]
+    implicates = [x for x in list(table.columns) if (not x in list_of_columns)] # at first : selects all columns
     for x in [0.00005, 0.0002, 0.001, 0.01, 0.1, 1]:
-        print 'currently testing with ratio : ', x
+#        print 'currently testing with ratio : ', x
         smaller_table = table[implicates + list_of_columns].iloc[:int(len(table) * x)]
-        implicates = smaller_table.groupby(list_of_columns).apply(lambda x: x[implicates].apply(lambda y: y.nunique() == 1)).apply(all)
-        implicates = list(implicates.index[implicates])        
+        col_is_implied = smaller_table.groupby(list_of_columns).apply(lambda x: x[implicates].apply(lambda y : y.nunique() == 1)).apply(all)
+        implicates = list(col_is_implied.index[col_is_implied]) # selects only columns that are predicted by list_of_columns
         if len(implicates) == 0:
             return []
     return implicates
    
 
 def get_all_predictions(table, list_of_columns, implications):
+    '''Returns prediction table'''
     return table[list_of_columns + implications].groupby(list_of_columns, as_index = False).apply(lambda x: x.iloc[0])
 
 
 def recreate_original_table(new_table, list_of_predictors):
-    '''Recreate full table with list'''
+    '''Recreate full table with the list_of_predictors'''
     for i in [len(list_of_predictors) - x - 1 for x in range(len(list_of_predictors))]:
         prediction = list_of_predictions[i]        
         print 'Adding :', [x for x in prediction['table'].columns if not(x in prediction['predictors'])], 'predicted by', prediction['predictors']
         new_table = new_table.merge(prediction['table'], on = prediction['predictors'], how = 'left')
     return new_table
 
+def add_original_columns(new_table, list_of_predictions, columns_to_add):
+    '''Add only the columns selected by columns_to_add'''
+    i = len(list_of_predictions) - 1
+    original_columns = list(new_table.columns)
+    predicted_columns = [y for x in list_of_predictions for y in x['predicted']]
+    if not all([col in predicted_columns for col in columns_to_add]):
+        print [col for col in columns_to_add if not(col in predicted_columns)], 'cannot be predicted'
+    else:
+        while (not all([col for col in columns_to_add if col in new_table.columns])) or i >= 0:
+            prediction = list_of_predictions[i]        
+            print 'Adding :', [x for x in prediction['table'].columns if not(x in prediction['predictors'])], 'predicted by', prediction['predictors']
+            new_table = new_table.merge(prediction['table'], on = prediction['predictors'], how = 'left')
+            i = i-1
+        if not all([col for col in columns_to_add if col in new_table.columns]):
+            print [col for col in columns_to_add if not(col in new_table.columns)], 'were not added'
+        return new_table[original_columns + list(columns_to_add)]
 
+def minimize_table(table, max_nb_predictors = 2, columns_to_analyse = None):
+    '''Returns the minimal table, and the associated predictors'''
+    if columns_to_analyse == None:
+        columns_to_analsye = list(table.columns)
+    
+    print 'Reformating table for exploration ...'
+    # Preperation of the table
+    new_table = table.copy()
+    nunique_by_col = new_table[columns_to_analyse].apply(lambda x: x.nunique())
+    nunique_by_col.sort(ascending = False)
+    columns = [x for x in new_table.columns if not(x in columns_to_analyse)] + list(nunique_by_col.index)
+    new_table = new_table[columns]
+    ind = list(new_table.index)
+    shuffle(ind)
+    new_table = new_table.loc[ind]
+    list_of_predictions = []
+    
+    # Tests all combinations of columns to see if they predict another variable
+    for nb_predictors in range(1,max_nb_predictors + 1):
+        print '\nCurrently testing', nb_predictors, 'predictors'
+        for list_of_columns in combinations([x for x in columns_to_analyse if x in new_table.columns], nb_predictors):
+            if all([x in new_table.columns for x in list_of_columns]):
+                print 'currently checking implications of', list_of_columns
+                all_implications = check_all_implications(new_table, list(list_of_columns))
+                if len(all_implications) != 0:
+                    print list_of_columns, '    >>> predicts', all_implications
+                    prediction = dict()
+                    prediction['predictors'] = list_of_columns
+                    prediction['predicted'] = all_implications
+                    prediction['table'] = get_all_predictions(table, list(list_of_columns), all_implications)
+                    list_of_predictions += [prediction]
+                    new_table.drop(all_implications, axis = 1, inplace = True)
+    return [new_table, list_of_predictions]
 
-print 'Reformating table for exploration ...'
-new_table = table.copy()
-nunique_by_col = new_table[columns_to_analyse].apply(lambda x: x.nunique())
-nunique_by_col.sort(ascending = False)
-columns = [x for x in new_table.columns if not(x in columns_to_analyse)] + list(nunique_by_col.index)
-new_table = new_table[columns]
+#if __name__ == '__main__':
+#    path = 'C:\\Users\\work\\Documents\\ETALAB_data'
+##    table = pd.read_csv(join(path, 'R201411.CSV'), sep = ';')
+#    columns_to_analyse = [col for col in table.columns if not(col in ['Unnamed: 0', 'rem_mon', 'rec_mon', 'dep_mon', 'act_dnb', 'act_coe'])]
+#    [new_table, list_of_predictions] = minimize_table(table, 2, columns_to_analyse)
 
-
-ind = list(new_table.index)
-shuffle(ind)
-new_table = new_table.loc[ind]
-list_of_predictions = []
-
-for nb_predictors in range(1,5):
-    print '\nCurrently testing', nb_predictors, 'predictors'
-    for list_of_columns in combinations([x for x in columns_to_analyse if x in new_table.columns], nb_predictors):
-        if all([x in new_table.columns for x in list_of_columns]):
-            print 'currently checking implications of', list_of_columns
-            all_implications = check_all_implications(new_table, list(list_of_columns))
-            if len(all_implications) != 0:
-                print list_of_columns, '    >>> predicts', all_implications
-                prediction = dict()
-                prediction['predictors'] = list_of_columns
-                prediction['table'] = get_all_predictions(table, list(list_of_columns), all_implications)
-                list_of_predictions += [prediction]
-                new_table.drop(all_implications, axis = 1, inplace = True)
-
-        
 #def check_implication(table, list_of_columns, variable_to_predict): 
 #    '''Checks if variable_to_predict is predicted by list_of_columns'''
 #    for x in [0.0002, 0.001, 0.01, 0.1, 1]:
